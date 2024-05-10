8cb2dfe69ba830e7fc7b16111c170203baec90d8
Diocechen <78402906+Diocechen@users.noreply.github.com>
2024-05-10 16:43:07 +0800

    Update C06_04.sql

--- 112_Database_Course_2-main/Assignment_6/C06_04.sql
+++ 112_Database_Course_2-main/Assignment_6/C06_04.sql
@@ -1,1 +1,14 @@
+--1
+SELECT *
+FROM employee
+WHERE employee_Add = '台南市東區崇明路110號';
 
+--2
+SELECT 
+    employee_id,
+    employee_Name,
+    LEFT(employee_Add, CHARINDEX('市', employee_Add)) AS city,
+    SUBSTRING(employee_Add, CHARINDEX('市', employee_Add) + 1, CHARINDEX('區', employee_Add) - CHARINDEX('市', employee_Add)) AS zone,
+    SUBSTRING(employee_Add, CHARINDEX('區', employee_Add) + 1, LEN(employee_Add)) AS address
+FROM employee
+WHERE employee_Add LIKE '%台南市%';
